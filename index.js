e4a1f024eb88dcf98ce67614f04b07a1bd4090bf
h2mil <om_msk@outlook.com>
2024-05-12 20:10:29 +0200

    fixed issue with AM PM
    setIntervall still not working

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 function updateTime() {
   let zurichElement = document.querySelector("#zurich");
-  let zurichDateElement = zurichElement.querySelector(".date");
-  let zurichTimeElement = zurichElement.querySelector(".time");
-  let zurichTime = moment().tz("Europe/Zurich");
+  if (zurichElement) {
+    let zurichDateElement = zurichElement.querySelector(".date");
+    let zurichTimeElement = zurichElement.querySelector(".time");
+    let zurichTime = moment().tz("Europe/Zurich");
 
-  zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
-  zurichTimeElement.innerHTML = zurichTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
+    zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+    zurichTimeElement.innerHTML = zurichTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 
   let bostonElement = document.querySelector("#boston");
-  let bostonDateElement = bostonElement.querySelector(".date");
-  let bostonTimeElement = bostonElement.querySelector(".time");
-  let bostonTime = moment().tz("America/New_York");
+  if (bostonElement) {
+    let bostonDateElement = bostonElement.querySelector(".date");
+    let bostonTimeElement = bostonElement.querySelector(".time");
+    let bostonTime = moment().tz("America/New_York");
 
-  bostonDateElement.innerHTML = bostonTime.format("MMMM Do YYYY");
-  bostonTimeElement.innerHTML = bostonTime.format(
-    "h:mm:ss[<small>]A[</small>]"
-  );
+    bostonDateElement.innerHTML = bostonTime.format("MMMM Do YYYY");
+    bostonTimeElement.innerHTML = bostonTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
 
   citiesElement.innerHTML = `<div class="city">
           <div>
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">
-            ${cityTime.format("hh:mm:ss[<small>]A[</small>]")}
-            <small>AM</small>
+            ${cityTime.format("hh:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small>
           </div>
         </div>
     `;
@@ -40,6 +46,5 @@ function updateCity(event) {
 
 updateTime();
 setInterval(updateTime, 1000);
-
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
