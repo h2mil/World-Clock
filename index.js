8cbf737057e58cddc8ff015a1acbc849ed0b90f7
h2mil <om_msk@outlook.com>
2024-05-12 19:55:42 +0200

    Added select element, real data
    some bugs not fixed yet, selected city has AM & PM showing, setInterval does not work

--- index.js
+++ index.js
@@ -20,5 +20,26 @@ function updateTime() {
   );
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityTimeZone}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">
+            ${cityTime.format("hh:mm:ss[<small>]A[</small>]")}
+            <small>AM</small>
+          </div>
+        </div>
+    `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
