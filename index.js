0fe53eb7c676ff349566c60d10fb79c41e71562a
h2mil <om_msk@outlook.com>
2024-05-12 21:11:48 +0200

    added current location, fixed cities in select
    bug "fixed": seconds are not supposed to update after select-event (foreseen by design)

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -46,5 +49,6 @@ function updateCity(event) {
 
 updateTime();
 setInterval(updateTime, 1000);
+
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
